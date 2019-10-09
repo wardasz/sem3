58e5fb575a10f23182f50a812154eeb1ca49f218
wardasz <m.wardzinski5991@gmail.com>
2019-10-09 13:12:37 +0200

    początek semestru

---
+++ grafy/lab1/zad1/grafy1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace grafy1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<List<int>> maciez = new List<List<int>>();
+          
+            var s = new FileInfo(Directory.GetCurrentDirectory());
+            var s2 = s.Directory.Parent.Parent;
+            String sciezka = s2.ToString() + "\\dane.csv";
+            
+            using (var reader = new StreamReader(sciezka))
+            {            
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    List<int> zad = new List<int>();
+                    for (int x=0; x<values.Length; x++)
+                    {
+                        zad.Add(Convert.ToInt32(values[x]));
+                    }
+                    maciez.Add(zad);
+
+                }
+            }
+            Console.WriteLine("Witam w analizatorze grafów by wardasz");
+
+            while (true)
+            {
+                switch (menu())
+                {
+                    case 49:
+                        wypisz(maciez);
+                        break;
+                    case 50:
+                        pazyste(maciez);
+                        break;
+                    default:
+                        Console.WriteLine("Zła liczba");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void pazyste(List<List<int>> maciez)
+        {
+            int pazyste = 0;
+            int niepazyste = 0;
+            foreach (List<int> zad in maciez)
+            {
+                int suma = 0;
+                foreach (int pole in zad)
+                {
+                    suma += pole;
+                }
+                if (suma % 2 == 0)
+                {
+                    pazyste++;
+                }
+                else
+                {
+                    niepazyste++;
+                }
+            }
+            Console.WriteLine(pazyste + " wierzchołków ma parzysty stopień, a " + niepazyste + " - nieparzysty.");
+        }
+
+        static void wypisz(List<List<int>> maciez)
+        {
+            foreach(List<int> zad in maciez)
+            {
+                Console.Write("[");
+                foreach (int pole in zad)
+                {
+                    Console.Write(pole+" ");
+                }
+                Console.Write("]\n");
+            }
+        }
+
+        static int menu()
+        {
+            Console.WriteLine("Co chcesz zrobić?");
+            Console.WriteLine("1-wypisz graf");
+            Console.WriteLine("2-sprawdź ile wieszchołków jest parzystych a ile nie");
+
+            int x = Convert.ToInt32(Console.Read());
+            Console.WriteLine(x);
+            return x;
+        }
+    }
+}
